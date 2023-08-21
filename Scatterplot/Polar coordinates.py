85fb115ba68b7f8b6c1cf0805a304c0e08ba5dbc
Zheyu Deng <96919656+Dzy2001@users.noreply.github.com>
2023-08-21 22:24:18 +0800

    Polar coordinates

---
+++ Scatterplot/Polar coordinates.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(19680801)
+
+N = 150
+r = 2 * np.random.rand(N)
+theta = 2 * np.pi * np.random.rand(N)
+area = 200 * r**2
+colors = theta
+
+#fig = plt.figure(figsize = (12, 12))
+#ax = fig.add_subplot(projection='polar')
+#c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
+#plt.savefig('极坐标散点图.png', dpi = 250)
+
+fig = plt.figure(figsize = (12, 12))
+ax = fig.add_subplot(projection='polar')
+c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
+
+ax.set_rorigin(-2.5)
+ax.set_theta_zero_location('W', offset=10)
+plt.savefig('polar coordinates.png', dpi = 250)
